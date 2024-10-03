de6c78537f21eee8afca85f54ad60ac8b27b3b30
sharath <sharathdevadiga210@gmail.com>
2024-10-03 12:53:56 +0530

    completed classes

--- classes/dist/index.js
+++ classes/dist/index.js
@@ -1,92 +1,43 @@
-//  _ -> prpotected, #-> private field
-
+"use strict";
+// public and private ony exists in typescript
+// by default -> everything is public. no need to specify, but for clarity
+// if there is no setter for the property then it becomes readonly
 class Player {
-  #score = 0;
-  numLives = 10;
-  static descreption = "Player in Our game";
-
-  constructor(first, last) {
-    this.first = first;
-    this.last = last;
-    console.log("In constructor");
-    this.#secret();
-  }
-  get fullName() {
-    return `${this.first} ${this.last}`;
-  }
-
-  get score() {
-    return this.#score;
-  }
-
-  set score(newSocre) {
-    if (newSocre > 0) this.#score = newSocre;
-    else console.log("scoremust be positive");
-  }
-
-  static randomPlayer() {
-    return new Player("Andy", "Samberg");
-  }
-
-  getScore() {
-    return this.#score;
-  }
-  setScore(score) {
-    if (score > 0) {
-      this.#score = score;
+    constructor(first, last) {
+        this.score = 0;
+        this.first = first;
+        this.last = last;
+        this.secretMethod();
     }
-  }
-
-  set fullname(newname) {
-    const [first, last] = newname.split(" ");
-    this.first = first;
-    this.last = last;
-  }
-
-  taunt() {
-    console.log("BOO!!");
-  }
-
-  looseLife() {
-    this.numLives -= 1;
-  }
-  #secret() {
-    console.log("secret");
-  }
+    secretMethod() {
+        console.log("New secret method");
+    }
 }
-
-const player1 = new Player("blue", "steel");
-player1.taunt();
-console.log(player1.first);
-console.log(player1.last);
-console.log(player1.numLives);
-player1.looseLife();
-console.log(player1.numLives);
-console.log(player1.getScore());
-player1.setScore(20);
-console.log(player1.fullName);
-console.log(player1.score);
-
-console.log(Player.descreption);
-console.log(Player.randomPlayer());
-
-player1.fullName = "adam jane";
-
-player1.score = 12;
-
-console.log(player1);
-
-const player2 = new Player("charlie", "brown");
-player2.taunt();
-
-// Inheritance
-class AdminPlayer extends Player {
-  constructor(first, last, powers) {
-    super(first, last);
-    this.powers = powers;
-  }
-  isAdmin = true;
+const rockey = new Player("rockey", "rock");
+// console.log(rockey.score);
+// PARAMETER PROPERTIES SHORTHAND
+class Player2 {
+    constructor(first, last, _score) {
+        this.first = first;
+        this.last = last;
+        this._score = _score;
+        this.secretMethod();
+    }
+    secretMethod() {
+        console.log("New secret method");
+    }
+    get fullname() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error("Score cannot be negative");
+        }
+        else {
+            this._score = newScore;
+        }
+    }
 }
-
-const adminPlayer1 = new AdminPlayer("admin", "mcAdmin");
-adminPlayer1.taunt();
--- classes/src/index.ts
+++ classes/src/index.ts
@@ -1,2 +1,126 @@
-console.log("Its working");
-console.log("WOO Yaa");
+// public, protected and private ony exists in typescript
+// by default -> everything is public. no need to specify, but for clarity
+// public is accissible everywhere
+// protected properties are only accessible by the children classes
+// private -> only in that class
+// if there is no setter for the property then it becomes readonly
+class Player {
+  public readonly first: string;
+  public readonly last: string;
+  private score: number = 0;
+
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+    this.secretMethod();
+  }
+
+  private secretMethod() {
+    console.log("New secret method");
+  }
+}
+
+const rockey = new Player("rockey", "rock");
+// console.log(rockey.score);
+
+// PARAMETER PROPERTIES SHORTHAND
+class Player2 {
+  constructor(
+    public first: string,
+    public last: string,
+    protected _score: number
+  ) {
+    this.secretMethod();
+  }
+
+  private secretMethod() {
+    console.log("New secret method");
+  }
+
+  get fullname(): string {
+    return `${this.first} ${this.last}`;
+  }
+
+  get score(): number {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) {
+      throw new Error("Score cannot be negative");
+    } else {
+      this._score = newScore;
+    }
+  }
+}
+
+class SuperPlayer extends Player2 {
+  isAdmin: boolean = true;
+  maxScore() {
+    this._score = 99999999;
+  }
+}
+
+// CLASSES AND INTERFACES -> implements
+// we can also have interface inheritance
+interface Colorful {
+  color: string;
+}
+
+interface Printable {
+  print(): void;
+}
+
+class Bike implements Colorful {
+  constructor(public color: string) {}
+}
+
+class Jacket implements Colorful, Printable {
+  constructor(public brand: string, public color: string) {}
+
+  print() {
+    console.log(`this jacket is a ${this.brand} branded with ${this.color}`);
+  }
+}
+
+const bike1 = new Bike("red");
+const jacket1 = new Jacket("Prada", "blue");
+
+// ABSTRACT CLASSES
+// cannot create a new instances from that class
+abstract class Employee {
+  constructor(public first: string, public last: string) {}
+  abstract getPay(): number; //all inheriting class must implement this method
+  greet() {
+    console.log("oooohhhh");
+  }
+}
+
+class FullTimeEmployee extends Employee {
+  constructor(first: string, last: string, private salary: number) {
+    super(first, last);
+  }
+  getPay() {
+    return this.salary;
+  }
+}
+
+class partTimeEmployee extends Employee {
+  constructor(
+    first: string,
+    last: string,
+    private hourleyRate: number,
+    private hoursWorked: number
+  ) {
+    super(first, last);
+  }
+  getPay() {
+    return this.hourleyRate * this.hoursWorked;
+  }
+}
+
+const betty = new FullTimeEmployee("betty", "Jack", 9555);
+console.log(betty.getPay());
+
+const bill = new partTimeEmployee("bill", "bell", 20, 500);
+console.log(betty.getPay());
